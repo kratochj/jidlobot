1d965278551bca1b9f24d6c3823b8423047ea2e6
Jiří Kratochvíl <jiri.kratochvil@finforce.cz>
2024-11-28 18:09:17 +0100

    Remove unused logger imports from JidlobotApplication.java
    This change eliminates redundant imports of SLF4J Logger and LoggerFactory from the JidlobotApplication.java file. Removing unused imports helps to keep the codebase clean and improves maintainability. No functional changes were made to the application.

--- src/main/java/eu/kratochvil/jidlobot/JidlobotApplication.java
+++ src/main/java/eu/kratochvil/jidlobot/JidlobotApplication.java
@@ -1,8 +1,6 @@
 package eu.kratochvil.jidlobot;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

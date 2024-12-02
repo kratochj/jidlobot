dc5b002915e13d1e0d1a5af1b0d718828ea3c99d
Jiří Kratochvíl <jiri.kratochvil@finforce.cz>
2024-12-02 16:54:50 +0100

    Refactor test setup and add new HTML test file
    - Introduced a setup method in the test class to initialize common objects, improving code reusability and reducing redundancy in test methods.
    - Created a new test HTML file and added a corresponding test method to increase test coverage.
    - The refactoring enhances maintainability and clarity of test code by extracting repetitive logic into a reusable method.

--- src/test/java/eu/kratochvil/jidlobot/DailyMenuParserTest.java
+++ src/test/java/eu/kratochvil/jidlobot/DailyMenuParserTest.java
@@ -4,11 +4,13 @@ import eu.kratochvil.jidlobot.model.DailyMenu;
 import eu.kratochvil.jidlobot.parser.DailyMenuParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,14 +21,28 @@ import static org.mockito.Mockito.when;
 public class DailyMenuParserTest {
     private static final Logger log = LoggerFactory.getLogger(DailyMenuParserTest.class);
 
-    @Test
-    void testParse() throws Exception {
-        log.debug("Starting testParse...");
-        JsoupConnector mockConnector = mock(JsoupConnector.class);
-        DailyMenuParser parser = new DailyMenuParser(mockConnector);
+    JsoupConnector mockConnector = null;
+    DailyMenuParser parser = null;
 
-        // Load the test HTML from resources
-        URL resource = getClass().getClassLoader().getResource("menus/testMenu001.html");
+    @BeforeEach
+    void setUp() {
+        mockConnector = mock(JsoupConnector.class);
+        parser = new DailyMenuParser(mockConnector);
+    }
+
+    @Test
+    void testParse001() throws Exception {
+        checkMenu(mockConnector, parser, "menus/testMenu001.html");
+    }
+
+    @Test
+    void testParse002() throws Exception {
+        checkMenu(mockConnector, parser, "menus/testMenu002.html");
+    }
+
+    private void checkMenu(JsoupConnector mockConnector, DailyMenuParser parser, String testFilename) throws IOException {
+        log.debug("Starting testParse...");
+        URL resource = getClass().getClassLoader().getResource(testFilename);
         assertNotNull(resource, "testMenu.html resource not found");
         String menuUrl = resource.toString();
 
